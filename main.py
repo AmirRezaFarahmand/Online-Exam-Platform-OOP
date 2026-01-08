5bb89e03191e22d884b72f0f9ee4a3ce38971cee
AmirReza Farahmand <amirreza85farahmand@gmail.com>
2026-01-08 13:05:27 +0330

    Add set_id method to User, Exam, Question Classes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from sys import implementation
-
-
 class User:
     def __init__(self, name, email):
         self.user_id = 0
         self.name = name
         self.email = email
+
+    def set_user_id(self, last_user_id):
+        self.user_id = last_user_id + 1
 
     def display_info(self):
         print('name: ', self.name, ', email: ', self.email)
@@ -38,14 +38,17 @@ class Student(User):
     def view_results(self):
         pass
 
-
 class Exam:
     def __init__(self, title, description, duration):
         self.exam_id = 0
         self.title = title
         self.description = description
         self.duration = duration
+        self.questions = []
         self.is_published = False
+
+    def set_exam_id(self, last_exam_id):
+        self.exam_id = last_exam_id + 1
 
     def add_question(self, question):
         pass
@@ -53,11 +56,17 @@ class Exam:
     def conduct_exam(self, student):
         pass
 
+    def change_published_status(self, status):
+        self.is_published = status
+
 class Question:
     def __init__(self, question_text, points):
         self.question_id = 0
         self.question_text = question_text
         self.points = points
+
+    def set_question_id(self, last_question_id):
+        self.question_id = last_question_id + 1
 
     def display(self):
         print(self.question_text)
@@ -80,6 +89,10 @@ class MCQ(Question):
         user_choice = int(user_choice)
         return user_choice == self.correct_option
 
+    def set_correct_option(self, correct_option):
+        correct_option = int(correct_option)
+        self.correct_option = correct_option
+
 class TFQ(Question):
     def __init__(self, question_text, points):
         super().__init__(question_text, points)
@@ -96,5 +109,14 @@ class TFQ(Question):
         else:
             user_answer = False
         return user_answer == self.correct_answer
+
+    def set_correct_answer(self, correct_answer):
+        if correct_answer == "T" or correct_answer == "t" \
+                or correct_answer == "true" or correct_answer == "True" \
+                or int(correct_answer) == 1:
+            correct_answer = True
+        else:
+            correct_answer = False
+        self.correct_answer = correct_answer
 
     
