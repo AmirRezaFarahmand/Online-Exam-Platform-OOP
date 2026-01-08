41eace3a67ec7d52d2042f2448186fc45f406721
AmirReza Farahmand <amirreza85farahmand@gmail.com>
2026-01-08 11:39:52 +0330

    Add Admin Class to main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from sys import implementation
+
+
 class User:
     def __init__(self, name, email):
         self.user_id = 0
@@ -7,3 +10,16 @@ class User:
     def display_info(self):
         print('name: ', self.name, ', email: ', self.email)
 
+class Admin(User):
+    def __init__(self, name, email):
+        super().__init__(name, email)
+        self.created_exams = []
+
+    def create_exam(self, title, duration):
+        pass
+
+    def add_question(self, question):
+        pass
+
+    def list_exams(self):
+        pass
